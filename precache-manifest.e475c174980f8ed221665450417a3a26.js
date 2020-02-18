c193b94cf2f41c892bcc6ee9536d82fdc3f71926
Jeus Santiago <jeus1santiago@gmail.com>
2020-02-18 18:51:56 -0500

    Updates

---
+++ precache-manifest.e475c174980f8ed221665450417a3a26.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "262028c41ea1a373672fa51a2c015b48",
+    "url": "/vpn-landing-page/index.html"
+  },
+  {
+    "revision": "dfb896e1fed8cb4cebd6",
+    "url": "/vpn-landing-page/static/css/main.42f45313.chunk.css"
+  },
+  {
+    "revision": "d14406fac256c3c008c7",
+    "url": "/vpn-landing-page/static/js/2.aa948238.chunk.js"
+  },
+  {
+    "revision": "a52d65c95a2d7ffcb9ff212c4d38dff4",
+    "url": "/vpn-landing-page/static/js/2.aa948238.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dfb896e1fed8cb4cebd6",
+    "url": "/vpn-landing-page/static/js/main.58624a92.chunk.js"
+  },
+  {
+    "revision": "fa84ebe1f12318650d35",
+    "url": "/vpn-landing-page/static/js/runtime-main.c42be5b5.js"
+  },
+  {
+    "revision": "be791618f5fbf8537863ddaf9fbf1622",
+    "url": "/vpn-landing-page/static/media/encryptedComp.be791618.png"
+  }
+]);
